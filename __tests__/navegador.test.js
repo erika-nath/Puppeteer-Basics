1f0ca32b82023caeb718eefed92a4b3b29043dc1
Nathaly <eri@MacBook-Air-de-Nathaly.local>
2024-12-21 15:10:24 -0600

    opciones de lanzamiento

--- __tests__/navegador.test.js
+++ __tests__/navegador.test.js
@@ -5,7 +5,16 @@ describe('Mi primer test en puppeteer',()=>{
 
         it('Debe abrir y cerrar el navegador',async () => {
             const browser = await puppeteer.launch({
-            headless:false
+            headless:false,  //uso de interfaz
+            slowMo:0,  // navegar lento
+            devtools:false, //abrir las devtools
+            /*defaultViewport:{
+                width:415,
+                height:915
+            } //tamaño de pagina */
+           // args:['--window-size = 415,915'] tamaño de ventana
+           defaultViewport:null  
+
         })
    
    
